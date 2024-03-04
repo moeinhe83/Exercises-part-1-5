15f8ab060ce00387eb1d25f4d98e280be75ddd87
Moein Heshmati <moeinheshmati1383@gmail.com>
2024-03-04 23:33:07 +0330

    Upload Exercises-part-1-5 project on Github

---
+++ README.md
@@ -0,0 +1,25 @@
+Project Name ===> Exercises Part 1
+
+Question Number ===> 5
+
+Programming Language ===> Python
+
+Program Name ===> The fifth exercise
+
+Example ===> Write a program that takes a number from the user and checks whether the number is divisible by 3 and 6
+
+Description Of Files:
+
+practice_5_linux.py ===> For Linux [Ubuntu Or Kali]
+
+For Run ===> python practice_5_linux.py
+
+practice_5_windows.py ===> For Windows
+
+For Run ===> python practice_5_windows.py
+
+Required packages For Python ===> os / termcolor2 / pyfiglet
+
+termcolor2 ==> To install => pip install termcolor2
+
+pyfiglet ==> To install => pip install pyfiglet
---
+++ practice_5_linux.py
@@ -0,0 +1,40 @@
+# Practice_5
+
+# Library
+from os import system as sys
+from termcolor2 import colored
+from pyfiglet import figlet_format
+
+# Clear Terminal
+sys('clear')
+
+# Intro
+print(colored(figlet_format("Exercises Part 1"), color='cyan'))
+print(colored('=====================================================================', color='red'))
+print(colored('Question Number ===> 5', color='blue'))
+print(colored('// Be Sure To Read The README File To See The Question //', color='blue'))
+print('                                            ')
+
+# Value Input
+number = int(input('Enter Number ==> '))
+
+# IF
+if number % 3 == 0 and number % 6 == 0:
+    print(colored('==================================='))
+    print(colored(f'{number} By 3 and 6', color='green'))
+
+elif number % 3 == 0:
+    print(colored('==================================='))
+    print(colored(f'{number} By 3', color='blue'))
+
+elif number % 6 == 0:
+    print(colored('==================================='))
+    print(colored(f'{number} By 6', color='yellow'))
+
+else:
+    print(colored('==================================='))
+    print(colored(f'{number} NOT By 3 and 6', color='red'))
+
+# Finish
+# Create By Moein Heshmati
+    
